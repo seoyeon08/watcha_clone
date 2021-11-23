deafc8aff0c4ebebc4c590ad9d3d6b0a4682d077
seoyeon <silversun0808@gmail.com>
2021-11-24 00:17:23 +0900

    Add: BeforeLoginScreen 세부 조정

--- src/screens/BeforeLoginScreen.js
+++ src/screens/BeforeLoginScreen.js
@@ -15,6 +15,7 @@ const Box = styled.div`
 const TextBox = styled.div`
   color: white;
   background: black;
+  margin-top: 160px;
 `;
 
 const ButtonBox = styled.div`
@@ -27,23 +28,28 @@ const LoginButton = styled.button`
   color: black;
   background: white;
   font-size: 0.8rem;
+  font-weight: bold;
   border-radius: 15px;
   padding: 5px;
   align-items: right;
   justify-content: center;
-  width: 70px;
+  width: 72px;
+  height: 30px;
   border: none;
+  margin-top: 20px;
 `;
 
 const StartButton = styled.button`
   color: white;
-  background: pink;
+  background: #f82f62;
   font-size: 1rem;
+  font-weight: bold;
   border-radius: 50px;
   border: none;
   justify-content: center;
   width: 170px;
   height: 50px;
+  margin-top: 40px;
 `;
 
 const DownButton = styled.button`
@@ -51,10 +57,12 @@ const DownButton = styled.button`
   background: none;
   border-radius: 30px;
   justify-content: center;
-  width: 30px;
-  height: 30px;
+  width: 40px;
+  height: 40px;
   border: 1px solid white;
-  margin-top: 80px;
+  margin-top: 90px;
+  font-size: 1.2rem;
+  font-weight: bold;
 `;
 
 const UpButton = styled.button`
@@ -65,7 +73,9 @@ const UpButton = styled.button`
   width: 30px;
   height: 30px;
   border: 1px solid white;
-  margin-top: 80px;
+  margin-top: 90px;
+  font-size: 1.2rem;
+  font-weight: bold;
 `;
 
 const BeforeLoginScreen = () => (
@@ -74,7 +84,7 @@ const BeforeLoginScreen = () => (
     <div id="container">
       <TextBox>
         <h1>영화, 드라마, 예능, 다큐멘터리를 무제한으로</h1>
-        <h3>매주 5백 여편의 신작이 업데이트 되며, 추가 요금은 전혀 없어요.</h3>
+        <p>매주 5백 여편의 신작이 업데이트 되며, 추가 요금은 전혀 없어요.</p>
       </TextBox>
       <ButtonBox>
         <StartButton>2주 무료 이용 시작</StartButton>
@@ -85,7 +95,7 @@ const BeforeLoginScreen = () => (
     <div id="container" >
       <TextBox>
         <h1>여럿이 함께, 하나의 이용권으로</h1>
-        <h3>동시 4개 기기에서 재생이 가능한 프리미엄 이용권을 이용해보세요.</h3>
+        <p>동시 4개 기기에서 재생이 가능한 프리미엄 이용권을 이용해보세요.</p>
       </TextBox>
       <ButtonBox>
         <StartButton>2주 무료 이용 시작</StartButton>
@@ -96,7 +106,7 @@ const BeforeLoginScreen = () => (
     <div id="container" backgroundImage = "url(back1.jpg)">
       <TextBox>
         <h1>이제 TV로 최고의 화질을 경험하세요</h1>
-        <h3>최대 Ultra HD 4K 해상도로 생생한 감동을 느껴보세요.</h3>
+        <p>최대 Ultra HD 4K 해상도로 생생한 감동을 느껴보세요.</p>
       </TextBox>
       <ButtonBox>
         <StartButton>2주 무료 이용 시작</StartButton>
@@ -107,7 +117,7 @@ const BeforeLoginScreen = () => (
     <div id="container">
       <TextBox>
         <h1>이동 중에도 감상을 멈추지 마세요</h1>
-        <h3>보고 싶은 콘텐츠를 다운로드하여 오프라인으로 즐기세요.</h3>
+        <p>보고 싶은 콘텐츠를 다운로드하여 오프라인으로 즐기세요.</p>
       </TextBox>
       <ButtonBox>
         <StartButton>2주 무료 이용 시작</StartButton>
@@ -118,7 +128,7 @@ const BeforeLoginScreen = () => (
     <div id="container">
       <TextBox>
         <h1>스마트폰, 태블릿, TV, PC, 크롬캐스트, Android TV에서</h1>
-        <h3>10만여 편의 작품을 무제한 스트리밍하세요.</h3>
+        <p>10만여 편의 작품을 무제한 스트리밍하세요.</p>
       </TextBox>
       <ButtonBox>
         <StartButton>2주 무료 이용 시작</StartButton>
