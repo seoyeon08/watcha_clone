739992b5e6d32d52fd8568941dddf9e09274b743
seoyeon <silversun0808@gmail.com>
2021-11-26 01:05:19 +0900

    Add: SignUpButton

---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const SignUpButton = styled.button`
+  color: black;
+  background: white;
+  font-size: 0.8rem;
+  font-weight: bold;
+  border-radius: 15px;
+  padding: 5px;
+  align-items: right;
+  justify-content: center;
+  width: 72px;
+  height: 30px;
+  border: none;
+  margin-top: 20px;
+`;
+
+const LoginScreen = () => (
+  <Box color="black">
+    <SignUpButton>회원가입</SignUpButton>
+  </Box>
+);
+
+export default LoginScreen;
